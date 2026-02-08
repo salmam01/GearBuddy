d0eba49b0b9e203c4bbccfcba1f008366ab71e32
salmam01 <if23b281@technikum-wien.at>
2026-02-08 16:23:46 +0100

    Set Up Discord Client

--- GearBuddy/Program.cs
+++ GearBuddy/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using DSharpPlus;
+using Microsoft.Extensions.Configuration;
+
+
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile("appsettings.local.json", optional: true, reloadOnChange: true)
+    .Build();
+
+string token = config["AuthToken"] ?? "unknown";
+
+DiscordClientBuilder builder = DiscordClientBuilder.CreateDefault(token, DiscordIntents.AllUnprivileged);
+DiscordClient client = builder.Build();
+
+await client.ConnectAsync();
+// Prevent console window from closing prematurely
+await Task.Delay(-1);
