52512cf5e21e944c42fa50e7e243875ee2149ce4
salmam01 <if23b281@technikum-wien.at>
2026-02-10 19:24:08 +0100

    Added DI container and basic command setup

--- GearBuddy/Program.cs
+++ GearBuddy/Program.cs
@@ -1,59 +1,91 @@
 ﻿using DSharpPlus;
 using DSharpPlus.Commands;
+using DSharpPlus.Commands.Processors.SlashCommands;
+using DSharpPlus.Commands.Processors.TextCommands;
 using GearBuddy.Configuration;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 class Program
 {
-    private static DiscordClient? Client { get; set; }
-    private static CommandsExtension? Commands { get; set; }
-
     static async Task Main(string[] args)
     {
+        // Service collection container
+        ServiceCollection services = new();
+
+        // Load the app configuration from the JSON files
         IConfiguration config = new ConfigurationBuilder()
             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
             .AddJsonFile("appsettings.local.json", optional: true, reloadOnChange: true)
             .Build();
 
-        DiscordConfig discordAppConfig = config
-            .GetSection("DiscordConfig")
-            .Get<DiscordConfig>()
-            ?? throw new Exception("Discord Configuration failed.");
+        services.AddSingleton(config);
+        
+        // Get code after compilation (or something like that idk)
+        Assembly currentAssembly = typeof(Program).Assembly;
 
-        if (string.IsNullOrWhiteSpace(discordAppConfig.AuthToken) ||
-            string.IsNullOrWhiteSpace(discordAppConfig.Prefix)
-        ) {
-            throw new Exception("Discord Configuration failed.");
-        }
+        try
+        {
+            DiscordConfig botConfig = config
+                .GetSection("DiscordConfig")
+                .Get<DiscordConfig>()
+                ?? throw new Exception("Discord Bot Configuration failed.");
 
-        // Todo: set up DiscordConfiguration
-
-        // Intents represent what data the bot is allowed to receive from guilds (servers)
-        DiscordIntents intents =
-            DiscordIntents.AllUnprivileged | 
-            DiscordIntents.MessageContents;
-
-        DiscordClientBuilder builder = DiscordClientBuilder.CreateDefault(
-            discordAppConfig.AuthToken, 
-            intents
-        );
-
-        // Handle incoming message event & respond to it
-        builder.ConfigureEventHandlers(
-            b => b.HandleMessageCreated(async (s, e) => 
+            if (string.IsNullOrWhiteSpace(botConfig.AuthToken))
             {
-                // Check if the message starts with the prefix
-                if (e.Message.Content.ToLower().StartsWith(discordAppConfig.Prefix)) 
-                {
-                    await e.Message.RespondAsync("I have no idea what I'm doing. Send help");
-                }
-            })
-        );
+                Console.Error.WriteLine("Discord AuthToken is missing.");
+                return;
+            }
 
-        Client = builder.Build();
+            if (string.IsNullOrWhiteSpace(botConfig.Prefix))
+            {
+                Console.Error.WriteLine("Bot Prefix is missing.");
+                return;
+            }
 
-        await Client.ConnectAsync();
-        await Task.Delay(-1);
+            // Register the Discord Client in the DI container
+            services.AddSingleton(serviceProvider =>
+            {
+                // Intents represent what data the bot is allowed to receive from guilds (servers)
+                DiscordIntents intents =
+                    DiscordIntents.AllUnprivileged |
+                    DiscordIntents.MessageContents;
+
+                DiscordClientBuilder builder = DiscordClientBuilder.CreateDefault(
+                    botConfig.AuthToken,
+                    intents,
+                    services
+                );
+
+                // Register the commands extension
+                builder.UseCommands(
+                    (serviceProvider, commands) =>
+                    {
+                        // Processors are input readers
+                        commands.AddProcessor(new TextCommandProcessor());
+                        commands.AddProcessor(new SlashCommandProcessor());
+                        commands.AddCommands(currentAssembly);
+                    }
+                );
+
+                // Returns a DiscordClient instance
+                return builder.Build();
+            });
+
+            IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            DiscordClient client = serviceProvider.GetRequiredService<DiscordClient>();
+
+            await client.ConnectAsync();
+            await Task.Delay(-1);
+
+        }
+        catch (Exception exception)
+        {
+            Console.Error.WriteLine($"An error occurred: {exception}");
+            return;
+        }
     }
 }
 
